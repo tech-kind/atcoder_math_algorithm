353582f69f1e0f10a9faf259190506b08a51fa82
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-20 14:44:31 +0900

    print_prime_numbers追加

---
+++ 011-print_prime_numbers/PrintPrimeNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= n; i++)
+        {
+            if (i == 2)
+            {
+                sb.Append($"{i}");
+                continue;
+            }
+
+            bool prime = true;
+            for (int j = 2; j < i; j++)
+            {
+                if ( i % j == 0 )
+                {
+                    prime = false;
+                }
+            }
+            if ( prime )
+            {
+                sb.Append($" {i}");
+            }
+        }
+
+        Console.WriteLine(sb.ToString());
+    }
+}
