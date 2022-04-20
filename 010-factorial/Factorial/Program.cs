af628b25d211585f7d7e2138085d039a93459de5
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-20 14:44:13 +0900

    factorial追加

---
+++ 010-factorial/Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        long result = Factorial(n);
+        Console.WriteLine($"{result}");
+    }
+
+    static long Factorial(int number)
+    {
+        if (number == 1)
+        {
+            return number;
+        }
+        else if (number >= 2)
+        {
+            return number * Factorial(number - 1);
+        }
+        return 0;
+    }
+}
