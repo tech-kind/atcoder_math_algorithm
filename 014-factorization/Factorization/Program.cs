7e120f64f06191dc94655eaf583430ac13fa0796
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-21 17:12:11 +0900

    factorization追加

---
+++ 014-factorization/Factorization/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+        Console.SetOut(sw);
+
+        long n = long.Parse(Console.ReadLine());
+
+        long tmp = n;
+        StringBuilder sb = new StringBuilder();
+        while(true)
+        {
+            double count = Math.Floor(Math.Sqrt(tmp));
+
+            bool isPrime = true;
+            for (int i = 2; i <= count; i++)
+            {
+                if (tmp % i == 0)
+                {
+                    sb.Append($"{i} ");
+                    tmp = tmp / i;
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                sb.Append($"{tmp}");
+                break;
+            }
+        }
+
+        Console.WriteLine(sb.ToString());
+        Console.Out.Flush();
+    }
+}
