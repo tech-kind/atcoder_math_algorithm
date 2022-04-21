c5a88a282254e6f79cd9e8242d79a1f1849f04dd
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-21 16:22:58 +0900

    primality_test追加

---
+++ 012-primality_test/PrimalityTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        long n = long.Parse(Console.ReadLine());
+
+        bool isPrime = true;
+        for (long i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+        string result = isPrime ? "Yes" : "No";
+        Console.WriteLine($"{result}");
+    }
+}
