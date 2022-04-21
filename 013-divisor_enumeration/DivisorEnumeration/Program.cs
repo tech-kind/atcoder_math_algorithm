52b59de3c9a81a16fc0d22b60d4eb180f902a6cc
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-21 17:11:57 +0900

    divisor_enumeration追加

---
+++ 013-divisor_enumeration/DivisorEnumeration/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+        Console.SetOut(sw);
+
+        long n = long.Parse(Console.ReadLine());
+
+        double count = Math.Floor(Math.Sqrt(n));
+        List<long> results = new List<long>();
+        for (int i = 1; i <= count; i++)
+        {
+            if (n % i == 0)
+            {
+                results.Add(i);
+                results.Add(n / i);
+            }
+        }
+
+        foreach (long result in results)
+        {
+            Console.WriteLine(result);
+        }
+
+        Console.Out.Flush();
+    }
+}
