24a681165ea857a6ca780f96bc91c052a057332c
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-22 15:09:19 +0900

    least_common_multiple_of_n_integers追加

---
+++ 017-least_common_multiple_of_n_integers/LeastCommonMultipleOfNIntegers/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] split = Console.ReadLine().Split(' ');
+
+        long[] array = new long[n];
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = long.Parse(split[i]);
+        }
+
+        long tmp = array[0];
+        for (int i = 1; i < n; i++)
+        {
+            long lcm = LCM(tmp , array [i]);
+            tmp = lcm;
+        }
+        Console.WriteLine($"{tmp}");
+    }
+
+    static long LCM(long ga, long gb)
+    {
+        long a = ga;
+        long b = gb;
+        while (a >= 1 && b >= 1)
+        {
+            if (a < b)
+                b = b % a;
+            else
+                a = a % b;
+        }
+        if (a >= 1)
+            return ga / a * gb;
+        else
+            return ga / b * gb;
+    }
+}
