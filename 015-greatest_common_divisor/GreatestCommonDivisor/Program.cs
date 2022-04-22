71e8f49337b069b3cbe49a4fbdfe8e2ca19f3479
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-22 15:08:40 +0900

    greatest_common_divisor追加

---
+++ 015-greatest_common_divisor/GreatestCommonDivisor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] split = Console.ReadLine().Split(' ');
+
+        long a = long.Parse(split[0]);
+        long b = long.Parse(split[1]);
+
+        long result = GCD(a, b);
+
+        Console.WriteLine($"{result}");
+    }
+
+    static long GCD(long a, long b)
+    {
+        while (a >= 1 && b >= 1)
+        {
+            if (a < b)
+                b = b % a;
+            else
+                a = a % b;
+        }
+        if (a >= 1)
+            return a;
+        else
+            return b;
+    }
+}
