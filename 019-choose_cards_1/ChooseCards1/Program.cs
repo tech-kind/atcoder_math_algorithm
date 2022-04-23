f3a9ec9c1c6807dbd4bb5cfeff1a253e91074be1
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-23 11:44:36 +0900

    choose_cards_1追加

---
+++ 019-choose_cards_1/ChooseCards1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] split = Console.ReadLine().Split(' ');
+
+        Span<long> vs = stackalloc long[3];
+        for(int i = 0; i < n; i++)
+        {
+            int value = int.Parse(split[i]);
+            vs[value % 3]++;
+        }
+
+        long result = nCk(vs[0], 2) + nCk(vs[1], 2) + nCk(vs[2], 2);
+        Console.WriteLine($"{result}");
+    }
+
+    /// <summary>
+    /// 組み合わせ (n >= k)
+    /// </summary>
+    static long nCk(long n, long k)
+    {
+        if (n < k) return 0;
+        if (n == k) return 1;
+
+        long x = 1;
+        for (long i = 0; i < k; i++)
+        {
+            x = x * (n - i) / (i + 1);
+        }
+        return x;
+    }
+}
