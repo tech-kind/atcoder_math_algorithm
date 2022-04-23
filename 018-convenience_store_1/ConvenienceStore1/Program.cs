b9f1ea55ccc10c9f185608902a9061cae1603ab9
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-23 11:44:25 +0900

    convenience_store_1追加

---
+++ 018-convenience_store_1/ConvenienceStore1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] split = Console.ReadLine().Split(' ');
+
+        Span<long> vs = stackalloc long[4];
+        for (int i = 0; i < n; i++)
+        {
+            int value = int.Parse(split[i]);
+            vs[value / 100 - 1]++;
+        }
+
+        long result = (vs[0] * vs[3]) + (vs[1] * vs[2]);
+        Console.WriteLine($"{result}");
+    }
+}
