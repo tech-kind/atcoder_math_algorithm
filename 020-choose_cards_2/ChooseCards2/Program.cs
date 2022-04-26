87d0f9eeb7f4470bf9d39285852310b96fd7e335
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-26 14:01:47 +0900

    choose_card_2追加

---
+++ 020-choose_cards_2/ChooseCards2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] split = Console.ReadLine().Split(' ');
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = int.Parse(split[i]);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                for (int k = j + 1; k < n; k++)
+                {
+                    for (int l = k + 1; l < n; l++)
+                    {
+                        for (int m = l + 1; m < n; m++)
+                        {
+                            if (array[i] + array[j] + array[k] + array[l] + array[m] == 1000)
+                                ans++;
+                        }
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine($"{ans}");
+    }
+}
