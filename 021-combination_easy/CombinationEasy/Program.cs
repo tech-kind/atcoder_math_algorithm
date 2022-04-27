4d36a0a6f48cd7b2cf792a3396e32fc574954491
TechKind <ken_hayakawa@tech-kind.biz>
2022-04-27 16:51:34 +0900

    combination_easy追加

---
+++ 021-combination_easy/CombinationEasy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] split = Console.ReadLine().Split(' ');
+
+        long n = long.Parse(split[0]);
+        long r = long.Parse(split[1]);
+
+        long result = nCk(n, r);
+        Console.WriteLine($"{result}");
+    }
+
+    /// <summary>
+    /// 組み合わせ (n >= k)
+    /// </summary>
+    static long nCk(long n, long k)
+    {
+        if (n < k) return 0;
+        if (n == k) return 1;
+
+        long x = 1;
+        for (long i = 0; i < k; i++)
+        {
+            x = x * (n - i) / (i + 1);
+        }
+        return x;
+    }
+}
