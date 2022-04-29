7d1de9d1496bf8ddd53ed6048a6df640dce20d96
tech-kind <ken_hayakawa@tech-kind.biz>
2022-04-29 13:10:10 +0900

    dice_expectation追加

---
+++ 023-dice_expectation/DiceExpectation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        string[] vs1 = Console.ReadLine().Split(' ');
+        string[] vs2 = Console.ReadLine().Split(' ');
+
+        int[] arrayB = new int[n];
+        int[] arrayR = new int[n];
+
+        for (int i = 0; i < n; i ++)
+        {
+            arrayB[i] = int.Parse(vs1[i]);
+            arrayR[i] = int.Parse(vs2[i]);
+        }
+
+        double expectB = 0;
+        double expectR = 0;
+        for (int i = 0; i < n; i++)
+        {
+            expectB += 1.0 * arrayB[i] / n;
+            expectR += 1.0 * arrayR[i] / n;
+        }
+
+        Console.WriteLine($"{expectB + expectR}");
+    }
+}
