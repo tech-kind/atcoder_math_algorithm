460cd854245a17c673055194ba5ae03eb5a9a426
tech-kind <ken_hayakawa@tech-kind.biz>
2022-04-30 05:31:08 +0000

    answer_exam_randomly追加

---
+++ 024-answer_exam_randomly/AnswerExamRandomly/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        double ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            string[] vs = Console.ReadLine().Split(' ');
+            int p = int.Parse(vs[0]);
+            int q = int.Parse(vs[1]);
+            ans += 1.0 * q / p;
+        }
+
+        Console.WriteLine($"{ans}");
+    }
+}
