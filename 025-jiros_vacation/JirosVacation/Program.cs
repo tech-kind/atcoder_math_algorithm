db3fbbb10293bf2497ee1db0614c22b4babc9f09
tech-kind <ken_hayakawa@tech-kind.biz>
2022-05-01 17:22:39 +0900

    jiros_vacation追加

---
+++ 025-jiros_vacation/JirosVacation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] arrayA = Console.ReadLine().Split(' ');
+        string[] arrayB = Console.ReadLine().Split(' ');
+
+        double ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int a = int.Parse(arrayA[i]);
+            int b = int.Parse(arrayB[i]);
+            ans += (1.0 * a / 3) + (1.0 * b * 2 / 3);
+        }
+
+        Console.WriteLine($"{ans}");
+    }
+}
