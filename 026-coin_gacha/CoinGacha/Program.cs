d780d783ffa23206e7c98acfe7761b148345a060
tech-kind <ken_hayakawa@tech-kind.biz>
2022-05-02 10:16:28 +0000

    coin_gacha追加

---
+++ 026-coin_gacha/CoinGacha/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        double ans = 1.0;
+        for (int i = 1; i < n; i++)
+        {
+            ans += 1.0 / (1.0 - (1.0 * i / n));
+        }
+
+        Console.WriteLine($"{ans}");
+    }
+}
