1f365d290e628bc0d8e9aa83873237b87eb0d252
tech-kind <ken_hayakawa@tech-kind.biz>
2022-05-03 04:21:09 +0000

    sorting追加

---
+++ 027-sorting/Sorting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] vs = Console.ReadLine().Split(' ');
+        long[] array = new long[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            array[i] = long.Parse(vs[i]);
+        }
+
+        Array.Sort(array);
+        
+        Console.WriteLine($"{string.Join(" ", array)}");
+    }
+}
