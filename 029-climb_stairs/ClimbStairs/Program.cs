81890f05d2b39f2d5a2525463b141e75890d8d25
tech-kind <ken_hayakawa@tech-kind.biz>
2022-05-05 08:10:29 +0000

    climb_stairs追加

---
+++ 029-climb_stairs/ClimbStairs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int[] dp = new int[n + 1];
+        for (int i = 0; i <= n; i++)
+        {
+            if (i <= 1)
+            {
+                dp[i] = 1;
+            }
+            else
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+        }
+
+        Console.WriteLine($"{dp[n]}");
+    }
+}
