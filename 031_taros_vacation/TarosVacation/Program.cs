62172d301ed7e713c7e2082020cd00ac99c4ff19
tech-kind <ken_hayakawa@tech-kind.biz>
2022-05-06 11:36:44 +0000

    taros_vacation

---
+++ 031_taros_vacation/TarosVacation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        string[] vs = Console.ReadLine().Split(' ');
+        long[] an = new long[n + 1];
+
+        for(int i = 1; i <= n; i++)
+        {
+            an[i] = long.Parse(vs[i - 1]);
+        }
+
+        long[] dp = new long[n + 1];
+        for (int i = 1; i <= n; i++)
+        {
+            if (i == 1 || i == 2)
+            {
+                dp[i] = an[i];
+            }
+            if (i >= 3)
+            {
+                dp[i] = Math.Max(dp[i - 1], dp[i - 2] + an[i]);
+            }
+        }
+
+        Console.WriteLine($"{dp[n]}");
+    }
+}
