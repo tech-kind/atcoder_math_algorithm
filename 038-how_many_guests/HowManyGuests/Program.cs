115262eabd95515b59de9a11c837aa1f9a8e05d4
tech-kind <ken_hayakawa@tech-kind.biz>
2022-05-13 07:55:57 +0000

    how_many_guests追加

---
+++ 038-how_many_guests/HowManyGuests/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var sw = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false };
+        Console.SetOut(sw);
+
+        string[] vs1 = Console.ReadLine().Split(' ');
+        int n = int.Parse(vs1[0]);
+        int q = int.Parse(vs1[1]);
+
+        string[] vs2 = Console.ReadLine().Split(' ');
+        long[] arrayA = new long[n + 1];
+        for (int i = 1; i <= n; i++)
+        {
+            arrayA[i] = long.Parse(vs2[i - 1]);
+        }
+
+        long[] arrayL = new long[q + 1];
+        long[] arrayR = new long[q + 1];
+        for (int i = 1; i <= q; i++)
+        {
+            string[] vs3 = Console.ReadLine().Split(' ');
+            arrayL[i] = long.Parse(vs3[0]);
+            arrayR[i] = long.Parse(vs3[1]);
+        }
+
+        long[] arrayB = new long[n + 1];
+        arrayB[0] = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            arrayB[i] = arrayB[i - 1] + arrayA[i];
+        }
+
+        for (int i = 1; i <= q; i++)
+        {
+            Console.WriteLine($"{arrayB[arrayR[i]] - arrayB[arrayL[i] - 1]}");
+        }
+
+        Console.Out.Flush();
+    }
+}
