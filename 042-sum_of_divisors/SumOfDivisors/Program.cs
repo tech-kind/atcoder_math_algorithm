3a9d18f47f02a8a0d9e0b94088d71fc709a532ab
tech-kind <ken_hayakawa@tech-kind.biz>
2022-05-25 06:53:13 +0000

    sum_of_divisors追加

---
+++ 042-sum_of_divisors/SumOfDivisors/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        long sum = 0;
+        for (long i = 1; i <= n; i++)
+        {
+            long x = n / i;
+            sum += i * x * (x + 1) / 2;
+        }
+
+        Console.WriteLine($"{sum}");
+    }
+}
