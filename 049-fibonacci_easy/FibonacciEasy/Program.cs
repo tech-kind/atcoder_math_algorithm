9603230e59aca2c4881a3dfba7a0ce52c368dfab
tech-kind <ken_hayakawa@tech-kind.biz>
2022-06-01 15:39:24 +0900

    fibonacci_easy追加

---
+++ 049-fibonacci_easy/FibonacciEasy/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        long[] array = new long[n + 1];
+        array[1] = 1;
+        array[2] = 1;
+
+        for (int i = 3; i <= n; i++)
+        {
+            array[i] = (array[i - 1] + array[i - 2]) % 1000000007;
+        }
+
+        Console.WriteLine($"{array[n] % 1000000007}");
+    }
+}
